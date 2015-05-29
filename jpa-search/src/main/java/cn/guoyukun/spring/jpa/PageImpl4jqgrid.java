a5e139c70550ef96b66bb942df5b33452dd011ac
郭玉昆 <gyk001@gmail.com>
2015-05-29 18:21:50 +0800

    修正jqgridpageimpl的属性名和jqgrid一致

--- jpa-search/src/main/java/cn/guoyukun/spring/jpa/PageImpl4jqgrid.java
+++ jpa-search/src/main/java/cn/guoyukun/spring/jpa/PageImpl4jqgrid.java
@@ -5,23 +5,23 @@ import java.util.List;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
-public class Page4jqgrid<T> extends PageImpl<T> {
+public class PageImpl4jqgrid<T> extends PageImpl<T> {
 	private static final long serialVersionUID = -4666797015040531454L;
 
-	public Page4jqgrid(List<T> content, Pageable pageable, long total) {
+	public PageImpl4jqgrid(List<T> content, Pageable pageable, long total) {
 		super(content, pageable, total);
 	}
 
-	public Page4jqgrid(List<T> content) {
+	public PageImpl4jqgrid(List<T> content) {
 		super(content);
 	}
 	
-	public long getTotal4jqgrid(){
+	public long getPage4jqgrid(){
 		long totalPages = this.getTotalPages();
 		return totalPages == 0 ? 1 : totalPages;
 	}
 
-	public long getNumber4jqgrid(){
+	public long getTotal4jqgrid(){
 		long number = this.getNumber();
 		return number == 0 ? 1 : number;
 	}
